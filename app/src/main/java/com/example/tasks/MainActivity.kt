98acfe500a2983709b8e214f947c57b998c59e50
lapadoga <ev.petrov@icloud.com>
2025-07-25 18:44:25 +0300

    Практический блок по Android: WorkManager для отправки уведомления о зарядке

--- app/src/main/java/com/example/tasks/MainActivity.kt
+++ app/src/main/java/com/example/tasks/MainActivity.kt
@@ -1,6 +1,15 @@
 package com.example.tasks
 
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.os.BatteryManager
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,6 +20,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.Worker
+import androidx.work.WorkerParameters
 import com.example.tasks.ui.theme.TasksTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,6 +43,8 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+        val checkChargeRequest = OneTimeWorkRequestBuilder<ChargingNotification>().build()
+        WorkManager.getInstance(applicationContext).enqueue(checkChargeRequest)
     }
 }
 
@@ -45,3 +63,54 @@ fun GreetingPreview() {
         Greeting("Android")
     }
 }
+
+class ChargingNotification(context: Context, workerParameters: WorkerParameters) :
+    Worker(context, workerParameters) {
+
+    private val context = applicationContext
+
+    override fun doWork(): Result {
+        val intentFilter = IntentFilter(Intent.ACTION_BATTERY_CHANGED)
+        val batteryAction = context.registerReceiver(null, intentFilter)
+
+        val status = batteryAction?.getIntExtra(BatteryManager.EXTRA_STATUS, -1)
+        val isCharging =
+            status == BatteryManager.BATTERY_STATUS_CHARGING || status == BatteryManager.BATTERY_STATUS_FULL
+
+        if (isCharging) {
+            sendNotification()
+        }
+
+        return Result.success()
+    }
+
+    private fun sendNotification() {
+        val notificationManager = NotificationManagerCompat.from(context)
+
+        val channelId = "Test channel"
+        val channelName = "Charge notification"
+
+        val notification = NotificationCompat.Builder(context, channelId)
+            .setContentTitle("Устройство на зарядке")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setAutoCancel(true)
+            .build()
+        val channel =
+            NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH)
+        val manager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        manager.createNotificationChannel(channel)
+
+        val isPermissionGranted = ActivityCompat.checkSelfPermission(
+            context,
+            Manifest.permission.POST_NOTIFICATIONS
+        ) == PackageManager.PERMISSION_GRANTED
+
+        if (!isPermissionGranted) {
+            Log.d("!!!!!", "Notifications are not allowed")
+        }
+
+        notificationManager.notify(0, notification)
+    }
+}
+
